56a1a0066a4e37a4102fa543d95e02a18c41f49f
carlos horacio lopez <carloshoracio.lopez@comafi.com.ar>
2023-11-28 20:28:26 -0300

    correccion

--- src/main/java/ar/com/codoacodo/controllers/NuevoOradorController.java
+++ src/main/java/ar/com/codoacodo/controllers/NuevoOradorController.java
@@ -1,18 +1,28 @@
 package ar.com.codoacodo.controllers;
 
+import java.io.IOException;
 import java.time.LocalDate;
 
 import ar.com.codoacodo.entity.Orador;
 import ar.com.codoacodo.repository.MySQLOradorRepository;
 import ar.com.codoacodo.repository.OradorRepository;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
-public class NuevoOradorController {
-
-	public static void main(String[] args) {
-		String nombre = "carlos";
-		String apellido = "lopez";
-		String email = "nue@mail.com";
-		String tema = "temax";
+public class NuevoOradorController extends HttpServlet{
+	//enviar por POST todos los datos desde el formulario en el front
+	
+	protected void doPost(
+			HttpServletRequest request,//aca viene todos lo del front 
+			HttpServletResponse response)  //aca va hacia el front
+	throws ServletException, IOException {
+		
+		String nombre = request.getParameter("nombre");//<input name="nombre">
+		String apellido = request.getParameter("apellido");
+		String email = request.getParameter("email");
+		String tema = request.getParameter("tema");
 		
 		//validaciones formato
 		if(nombre == null || apellido == null || email == null || tema == null) {
